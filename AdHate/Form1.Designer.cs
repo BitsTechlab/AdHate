1fa2c0acbca3865e82d688031458ff6cba1f80de
Syrexide <wwc.302@gmail.com>
2016-02-06 12:58:37 -0500

    initial release

---
+++ AdHate/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace AdHate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(257, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "block";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(260, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "an application that modifies the hosts file to block a majority of the ads on the" +
+    " internet";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(257, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "unblock";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 100);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(257, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(15, 129);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(257, 120);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "AdHate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ AdHate/Form1.cs
@@ -0,0 +1,174 @@
+﻿using AdHate.Properties;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AdHate
+{
+    public partial class Form1 : Form
+    {
+        private string etc = Environment.GetFolderPath(Environment.SpecialFolder.System) + @"\drivers\etc\";
+
+        public Form1()
+        {
+            InitializeComponent();
+            // backup hosts
+            if (File.Exists(etc + "hosts") && Settings.Default.firstrun == false || !File.Exists(etc + "hosts.bak"))
+            {
+                File.Copy(etc + "hosts", etc + "hosts.bak", true);
+                Settings.Default.firstrun = true;
+                Settings.Default.Save();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // just in case
+                textBox1.ForeColor = Color.White;
+                button1.Enabled = false;
+                button2.Enabled = false;
+
+                // take ownership of hosts
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.WorkingDirectory = etc;
+                startInfo.Arguments = "/c takeown /f hosts && icacls hosts /grant administrators:F";
+                process.StartInfo = startInfo;
+                process.Start();
+
+                textBox1.Text = "Took ownership of hosts file";
+
+                progressBar1.Value = 10;
+
+                // get mega-hosts file
+                StringBuilder sb = new StringBuilder();
+
+                byte[] buf = new byte[8192];
+
+                HttpWebRequest request = (HttpWebRequest)
+                WebRequest.Create("https://cdn.rawgit.com/StevenBlack/hosts/master/hosts");
+
+                HttpWebResponse response = (HttpWebResponse)
+                request.GetResponse();
+
+                Stream resStream = response.GetResponseStream();
+
+                string tempString = null;
+                int count = 0;
+
+                do
+                {
+                    count = resStream.Read(buf, 0, buf.Length);
+
+                    if (count != 0)
+                    {
+                        tempString = Encoding.ASCII.GetString(buf, 0, count);
+
+                        sb.Append(tempString);
+                    }
+                }
+                while (count > 0);
+
+                textBox1.Text = textBox1.Text + "\r\nGot mega-hosts file (by StevenBlack)";
+
+                progressBar1.Value = 80;
+
+                // write to hosts
+                File.WriteAllText(etc + "hosts", sb.ToString());
+
+                textBox1.Text = textBox1.Text + "\r\nWritten to hosts file";
+
+                progressBar1.Value = 90;
+
+                // flush dns
+                startInfo.Arguments = "/c ipconfig /flushdns";
+                process.StartInfo = startInfo;
+                process.Start();
+
+                textBox1.Text = textBox1.Text + "\r\nFlushed DNS cache\r\nDone.";
+
+                textBox1.ForeColor = Color.Lime;
+
+                progressBar1.Value = 100;
+
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+            // if bad things happen
+            catch (Exception ex)
+            {
+                textBox1.ForeColor = Color.Red;
+                textBox1.Text = textBox1.Text + "\r\nWARNING: " + Convert.ToString(ex);
+                textBox1.Text = textBox1.Text + "\r\nNo changes to your system have been made.";
+                progressBar1.Value = 0;
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // just in case
+                textBox1.ForeColor = Color.White;
+                button1.Enabled = false;
+                button2.Enabled = false;
+
+                // restore hosts
+                progressBar1.Value = 0;
+
+                if (File.Exists(etc + "hosts"))
+                {
+                    File.Delete(etc + "hosts");
+                }
+
+                File.Copy(etc + "hosts.bak", etc + "hosts", true);
+
+                progressBar1.Value = 90;
+
+                textBox1.Text = "Restored hosts";
+
+                // flush dns
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c ipconfig /flushdns";
+                process.StartInfo = startInfo;
+                process.Start();
+
+                textBox1.Text = textBox1.Text + "\r\nFlushed DNS cache\r\nDone.";
+
+                textBox1.ForeColor = Color.Lime;
+
+                progressBar1.Value = 100;
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+            // if more bad things happen
+            catch (Exception ex)
+            {
+                textBox1.ForeColor = Color.Red;
+                textBox1.Text = textBox1.Text + "\r\n\r\nWARNING: " + Convert.ToString(ex);
+                textBox1.Text = textBox1.Text + "\r\nNo changes to your system have been made.";
+                progressBar1.Value = 0;
+                button1.Enabled = true;
+                button2.Enabled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.SelectionStart = textBox1.Text.Length;
+            textBox1.ScrollToCaret();
+        }
+    }
+}
